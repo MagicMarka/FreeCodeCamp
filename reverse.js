641ab1605db55925b7c5ea0b110e1649fa80ee8c
MagicMarka <magicformarka@gmail.com>
2017-02-02 13:43:58 +0200

    add file

---
+++ reverse.js
@@ -0,0 +1,13 @@
+
+
+// Reverse the provided string.
+
+
+function reverseString(str) {
+	var arr = str.split("");
+	arr = arr.reverse();
+	str = arr.join("");
+  	return str;
+}
+
+reverseString("hello");
